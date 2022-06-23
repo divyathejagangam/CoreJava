4b05aedc293cf8dcba4891ba4f2dbe2473369d57
divyathejagangam <99860702+divyathejagangam@users.noreply.github.com>
2022-06-23 17:31:38 +0530

    Print statements

---
+++ First.java
@@ -0,0 +1,13 @@
+//Print Statements
+package com.edu;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Divya Theja");
+		System.out.println("Satya Enclave, Chandanagar, Hyderabad, 500050");
+
+	}
+
+}
